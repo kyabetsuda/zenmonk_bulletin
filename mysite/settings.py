610285729932cdf51cfa56004b10758869954e49
kyabetsuda <junn135246@icloud.com>
2019-04-21 22:32:00 +0900

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '805ibo%m9t3_0_(u@j2x+r!_t5xie@z1bmz+6u=j)0b-6*n&=('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['133.167.42.228']
+ALLOWED_HOSTS = ['133.167.42.228','127.0.0.1']
 
 
 # Application definition
