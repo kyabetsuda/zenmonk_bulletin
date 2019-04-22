f0ffda17951523cf7213684a7cc0ad08c94bae87
kyabetsuda <junn135246@icloud.com>
2019-04-22 23:39:33 +0900

    commit

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -9,10 +9,12 @@ app_name = 'bulletin'
 urlpatterns = [
     # ex: /
     path('', views.index, name='index'),
-
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     # # ex: /post/create/
     # path('post/create/', views.create, name='create'),
-    # 
+    #
     # # ex: /post/1/
     # path('post/<int:pk>/', views.detail, name='detail'),
     #
