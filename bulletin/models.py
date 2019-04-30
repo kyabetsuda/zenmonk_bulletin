82c20358b9bc3b02b9644ba758513fc3bc3dfc1b
kyabetsuda <junn135246@icloud.com>
2019-04-30 23:50:48 +0900

    commit

--- bulletin/models.py
+++ bulletin/models.py
@@ -17,3 +17,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    postid = models.ForeignKey('bulletin.Post', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
