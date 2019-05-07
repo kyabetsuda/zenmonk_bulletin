6f14aa98f413b788552b9f21ddbfba7c17088c45
kyabetsuda <junn135246@icloud.com>
2019-05-07 22:34:14 +0900

    commit

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -11,9 +11,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
     path('post/<int:pk>/comment/new', views.comment_new, name='comment_new'),
     path('post/<int:pk>/comment/<int:commentid>/edit', views.comment_edit, name='comment_edit'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('mypage/', views.mypage, name='mypage'),
+    path('mypage/article_list', views.article_list, name='article_list'),
     # # ex: /post/create/
     # path('post/create/', views.create, name='create'),
     #
--- bulletin/views.py
+++ bulletin/views.py
@@ -7,10 +7,12 @@ from django.utils import timezone #<- 追記
 from .models import Post, Comment
 from .services.LoggerService import LoggerService as ls
 from .services.AccessService import AccessService
+from django.contrib.auth.models import User
+from django.http import Http404
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.order_by('-published_date').all()
+    posts = Post.objects.order_by('-published_date').all().exclude(draft_flg='1')
     return render(request, 'bulletin/index.html', {'posts': posts})
 
 @login_required
@@ -32,6 +34,10 @@ def post_new(request):
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # 記事の作者がログインユーザと違う場合はエラー
+    if post.author != request.user:
+        raise Http404
+
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -39,7 +45,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.publish()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('/post/' + str(pk), pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'bulletin/post_edit.html', {'form': form})
@@ -54,9 +60,9 @@ def post_detail(request, pk):
 
 @login_required
 def comment_new(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
-        post = get_object_or_404(Post, pk=pk)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.author = request.user
@@ -72,6 +78,10 @@ def comment_new(request, pk):
 def comment_edit(request, pk, commentid):
     comment = get_object_or_404(Comment, pk=commentid)
     post = get_object_or_404(Post, pk=pk)
+    # コメントの作者が違う場合はエラー
+    if comment.author != request.user:
+        raise Http404
+
     if request.method == "POST":
         form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
@@ -83,4 +93,25 @@ def comment_edit(request, pk, commentid):
             return redirect('/post/' + str(pk), pk=pk)
     else:
         form = CommentForm(instance=comment)
-    return render(request, 'bulletin/comment_edit.html', {'form': form})
+    return render(request, 'bulletin/comment_edit.html', {'form': form })
+
+@login_required
+def mypage(request):
+    user = get_object_or_404(User, pk=request.user.id)
+    return render(request, 'bulletin/mypage.html')
+
+@login_required
+def article_list(request):
+    user = get_object_or_404(User, pk=request.user.id)
+    posts = Post.objects.filter(author=user)
+    return render(request, 'bulletin/article_list.html', {'posts': posts })
+
+@login_required
+def post_delete(request, pk):
+    user = get_object_or_404(User, pk=request.user.id)
+    post = get_object_or_404(Post, pk=pk)
+    # 記事の作者がログインユーザと違う場合はエラー
+    if post.author != request.user:
+        raise Http404
+    post.delete()
+    return redirect('/')
