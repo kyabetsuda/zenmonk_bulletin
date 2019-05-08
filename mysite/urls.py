057e1a16462b7428c4c759413a33e77cd0d315cd
kyabetsuda <junn135246@icloud.com>
2019-05-08 20:52:53 +0900

    commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-from mysite import settings
+# from django.conf.urls.static import static
+# from mysite import settings
 
 # from mysite import settings
 # from . import views
@@ -30,4 +30,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
