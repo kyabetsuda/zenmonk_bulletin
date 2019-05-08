2b354ae3a0a0ddf20ad5afdffa077395c2116db0
kyabetsuda <junn135246@icloud.com>
2019-05-08 20:23:27 +0900

    commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+# from mysite import settings
 # from . import views
 
 app_name = 'mysite'
